4f923888c8cff51461c3f38368821327c7530dc8
Jay Panchal <panchalj272@gmail.com>
2022-07-02 18:29:18 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from "./components/Header"
-import Meme from "./components/Meme"
 import Memes from "./components/Memes"
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      {/* <Meme /> */}
       <Memes />
     </div>
   );
